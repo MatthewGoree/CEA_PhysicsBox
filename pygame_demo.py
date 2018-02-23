c85502910f8caa377635da372bcdd14e4832c854
MatthewGoree <matthewgoree@utexas.edu>
2018-02-22 19:47:11 -0600

    Update pygame_demo.py

--- pygame_demo.py
+++ pygame_demo.py
@@ -1,13 +1,16 @@
-# Conversions:
-# atm 100 pixels = 1 m; 1 iteration = 1 sec
+# Conventions:
+# atm 1 pixel = 1 m; 1 iteration = 1 sec
+# Decide on uniform framerate?
 
-import sys, pygame, numpy
+import sys, pygame
+import numpy as np
 from properties import *
+from objects import *
 clock = pygame.time.Clock()
 pygame.init()
 
 size = width, height = 600, 600
-ball_size = 100, 100 # Must be even to have an int radius
+ball_size = 100, 100 # Must be even to have an int ball.radius
 ### TO GET RID OF LIQUID SET THIS TO ZERO ###
 amount_liquid = 600*600*.4
 liquid_height = height - amount_liquid / width # pixels
@@ -25,13 +28,15 @@ screen = pygame.display.set_mode(size)
 
 ball = pygame.image.load("ball.png")
 ball = pygame.transform.scale(ball, ball_size)
-radius = int(ball_size[0] / 2)
-volume = 4 / 3 * numpy.pi * radius**3
+radius = 50
 
-buoyancy = volume * gravity * liquid.density
-mass = material.density * volume
-ballrect = ball.get_rect()
 
+#ball.mass = material.density * ball.volume
+# ball.rect = ball.get_rect()
+
+ball = MassObject("ball.png", 50, copper)
+
+#buoyancy = ball.volume * gravity * liquid.density
 
 while 1:
 
@@ -41,56 +46,56 @@ while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    ballrect = ballrect.move(tmove)
+    ball.rect = ball.rect.move(tmove)
 
     # Makes ball bounce off walls
-    if ballrect.left <= 0 or ballrect.right >= width:
+    if ball.rect.left <= 0 or ball.rect.right >= width:
         speed[0] = -material.CoR * speed[0]
-    if ballrect.top <= 0 or ballrect.bottom >= height:
+    if ball.rect.top <= 0 or ball.rect.bottom >= height:
         speed[1] = -material.CoR * speed[1]
 
     # Adds acceleration due to gravity
-    if ballrect.top >= 0:
+    if ball.rect.top >= 0:
         speed[1] = speed[1] + gravity
 
     # Buoyancy and drag force in liquid
-    if ballrect.bottom > liquid_height:
-        # Allows for buoyancy/drag of partially submerged masses
-        if ballrect.top < liquid_height:
-            h = ballrect.bottom - liquid_height
-            if radius >= h:
-                r_submerged = (2*h*radius - h**2)**(1/2)
-                vol_submerged = numpy.pi * h ** 2 / 3 * (3 * radius - h)
+    if ball.rect.bottom > liquid_height:
+        # Allows for buoyancy/drag of partially submerged ball.masses
+        if ball.rect.top < liquid_height:
+            h = ball.rect.bottom - liquid_height
+            if ball.radius >= h:
+                r_submerged = (2*h*ball.radius - h**2)**(1/2)
+                vol_submerged = np.pi * h ** 2 / 3 * (3 * ball.radius - h)
                 # Central angle of partial circle
-                theta = 2 * numpy.arccos((radius - h) / radius)
+                theta = 2 * np.arccos((ball.radius - h) / ball.radius)
             else:
-                h_temp = 2 * radius - h
-                r_submerged = 2 * numpy.tan(numpy.arccos((h - radius) / radius)) * (h - radius)
-                vol_submerged = volume - numpy.pi * h_temp ** 2 / 3 * (3 * radius - h_temp)
+                h_temp = 2 * ball.radius - h
+                r_submerged = 2 * np.tan(np.arccos((h - ball.radius) / ball.radius)) * (h - ball.radius)
+                vol_submerged = ball.volume - np.pi * h_temp ** 2 / 3 * (3 * ball.radius - h_temp)
                 # Central angle of partial circle
-                theta = 2 * numpy.arccos(h_temp / radius)
-            #area_submerged = numpy.pi * r_submerged**2
+                theta = 2 * np.arccos(h_temp / ball.radius)
+            #area_submerged = np.pi * r_submerged**2
             # Ratio of area submerged to total area
-            ratio = (radius**2 / 2 * (theta - numpy.sin(theta))) / (numpy.pi * radius**2)
+            ratio = (ball.radius**2 / 2 * (theta - np.sin(theta))) / (np.pi * ball.radius**2)
             buoy_submerged = vol_submerged * liquid.density * gravity
-            acc_b = buoy_submerged / mass
+            acc_b = buoy_submerged / ball.mass
             # Modified Stokes' flow
-            Fdx = 6 * numpy.pi * liquid.viscosity * radius * speed[0] * ratio
-            Fdy = 6 * numpy.pi * liquid.viscosity * r_submerged * speed[1]
+            Fdx = 6 * np.pi * liquid.viscosity * ball.radius * speed[0] * ratio
+            Fdy = 6 * np.pi * liquid.viscosity * r_submerged * speed[1]
         else:
-            acc_b = buoyancy / mass
+            acc_b = buoyancy(ball, liquid, gravity) / ball.mass
             # Stokes' flow
-            Fdx = 6 * numpy.pi * liquid.viscosity * radius * speed[0]
-            Fdy = 6 * numpy.pi * liquid.viscosity * radius * speed[1]
-        if ballrect.bottom == height:
+            Fdx = 6 * np.pi * liquid.viscosity * ball.radius * speed[0]
+            Fdy = 6 * np.pi * liquid.viscosity * ball.radius * speed[1]
+        if ball.rect.bottom == height:
             if speed[0] > 0:
-                acc_fric = friction * (gravity - buoyancy/mass)
+                acc_fric = friction * gravity
             else:
-                acc_fric = -friction * (gravity - buoyancy/mass)
+                acc_fric = -friction * gravity
         else:
             acc_fric = 0
-        acc_dx = Fdx / mass
-        acc_dy = Fdy / mass
+        acc_dx = Fdx / ball.mass
+        acc_dy = Fdy / ball.mass
         speed[0] = speed[0] - acc_dx - acc_fric
         speed[1] = speed[1] - acc_dy - abs(acc_b)
 
@@ -98,26 +103,28 @@ while 1:
     tmove[0] = speed[0]
     tmove[1] = speed[1]
     if speed[0] < 0:
-        if speed[0] < -ballrect.left:
-            tmove[0] = -ballrect.left
+        if speed[0] < -ball.rect.left:
+            tmove[0] = -ball.rect.left
     else:
-        x_dist_right = width - ballrect.right
+        x_dist_right = width - ball.rect.right
         if speed[0] > x_dist_right:
             tmove[0] = x_dist_right
     if speed[1] < 0:
-        if speed[1] < -ballrect.top:
-            tmove[1] = -ballrect.top
+        if speed[1] < -ball.rect.top:
+            tmove[1] = -ball.rect.top
     else:
-        y_dist_bottom = height - ballrect.bottom
+        y_dist_bottom = height - ball.rect.bottom
         if speed[1] > y_dist_bottom:
             tmove[1] = y_dist_bottom
 
 
+
+
     screen.fill(black)
     pygame.draw.rect(screen, liquid.color, (0, liquid_height, width, height))
-    color_surface(ball, material.color)
+    color_surface(ball.image, material.color)
     # Adds a black border to the circle
-    pygame.draw.circle(ball, (0,0,0), (radius,radius), radius, 2)
-    screen.blit(ball, ballrect)
+    pygame.draw.circle(ball.image, (0,0,0), (ball.radius,ball.radius), ball.radius, 2)
+    screen.blit(ball.image, ball.rect)
     pygame.display.flip()
     clock.tick()
